be2ce3979532f4e457d56c40d1f104a3cea8641a
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2023-04-20 16:19:12 -0400

    feat(frontend-backend): Create frontend and backend for the project (#1)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { Box, Heading } from "@chakra-ui/react"
+import React from "react"
+
+export default function App() {
+	return (
+		<>
+			<Box w='100vw' h='100vh'>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+    
+				<Heading > This Is Sparta!! </Heading>
+			</Box>
+		</>
+	)
+}
